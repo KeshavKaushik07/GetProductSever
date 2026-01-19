4142f82eff88893af546d3022b4acca5bd192344
Keshav Kaushik <keshavkaushik955@gmail.com>
2026-01-19 19:40:17 +0530

    add Server

--- server.js
+++ server.js
@@ -7,29 +7,29 @@ const cors = require("cors");
 dotenv.config();
 
 // connect to DB
-connectDB();
+// connectDB();
 let isConnected = false;
-// async function connectMongoDB() {
-//     try{
-//         await connectDB();
-//         isConnected = true;
-//         console.log("connected to DB");
-//     }catch(err){
-//         console.log("error in connecting to DB",err);
-//     }
-// }
+async function connectMongoDB() {
+    try{
+        await connectDB();
+        isConnected = true;
+        console.log("connected to DB");
+    }catch(err){
+        console.log("error in connecting to DB",err);
+    }
+}
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-// app.use((req,resp,next)=>{
-//     if(!isConnected)
-//     {
-//         connectMongoDB();
-//     }
-//     next();
-// })
+app.use((req,resp,next)=>{
+    if(!isConnected)
+    {
+        connectMongoDB();
+    }
+    next();
+})
 
 
 app.use("/api/v1/test",require("./Routes/testRoute"));
@@ -38,7 +38,7 @@ app.use("/api/v1/user",require("./Routes/userRoutes"));
 
 const port = process.env.PORT || 4000;
 
-app.listen(port,()=>{
-    console.log(`server is listning on port ${port}`.bgMagenta);
-})
-// module.exports = app;
+// app.listen(port,()=>{
+//     console.log(`server is listning on port ${port}`.bgMagenta);
+// })
+module.exports = app;
